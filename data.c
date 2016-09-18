64bb9a08ddf7f56c0fc5412a33969a2e4f81254c
omipurandare <omkar.purandare@colorado.edu>
2016-09-17 18:56:44 -0600

    modified data

--- data.c
+++ data.c
@@ -7,46 +7,37 @@ void dump_memory(uint8_t * start, uint32_t length)
   int i;
    for(i=0;i<length;i++)
     {
-        printf("%x\t",*start);
+        printf("%x\t",*start); //printing out the hex values of the memory places
         start++;
     }
 }
 
 uint32_t big_to_little(uint32_t data)
 {
- uint8_t * start1,temp,i,j;
-    j=(uint8_t)data;
-    start1=&j;
-    for(i=0;i<2;i++)
-    {
-        temp=*(start1+i);
-     *(start1+i)=*(start1+3-i);
-     *(start1+3-i)=temp;
-        start1++;
-    }
+ data=((num>>24)&0xff) //moves bits 24 places right and removes first 8 bits
+  |((num<<8)&0xff0000) //moves bits 8 places left and removes 16 to 24 bits
+  |((num>>8)&0xff00) //moves bits 8 places right and removes 8 to 16 bits
+  |((num<<24)&0xff000000); // moves bits 24 places left and removes last 8 bits
+  // ORing all of them gives the new 32bits reversed, which finishes the conversion
     return data;
 }
 
 uint32_t little_to_bit(uint32_t data)
 {
- uint8_t * start2,temp,i,j;
-    j=(uint8_t)data;
-    start2=&j;
-    for(i=0;i<2;i++)
-    {
-        temp=*(start2+i);
-     *(start2+i)=*(start2+3-i);
-     *(start2+3-i)=temp;
-        start2++;
-    }
+ 
+ data=((num>>24)&0xff) //moves bits 24 places right and removes first 8 bits
+  |((num<<8)&0xff0000) //moves bits 8 places left and removes 16 to 24 bits
+  |((num>>8)&0xff00) //moves bits 8 places right and removes 8 to 16 bits
+  |((num<<24)&0xff000000); // moves bits 24 places left and removes last 8 bits
+  // ORing all of them gives the new 32bits reversed, which finishes the conversion
     return data;
 }
 
-int32_t my_atoi(int8_t * str)
+int32_t my_atoi(int8_t * str) //getting the pointer to first character
 {
-  int32_t* whole,j;
-   whole=(int32_t*)str;
-   return *whole;
+  int32_t* whole; //32 bit interger pointer
+   whole=(int32_t*)str; //type casting 8bit pointer to 16 bit
+   return *whole; //returning 32 bits
 }
 
 
